1dda7e8508e06d374bb28c0a34da8af494b41dbe
MarcosGarlot <garlotmarcos@gmail.com>
2021-08-23 19:43:26 -0300

    Desafio 3: Menú e-commerce - Tienda eco + Garlot

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+ function NavBar() {
+    return (
+        <div >
+            <h1>ECOrdoba</h1>
+            <p>Nosotros</p>
+            <p>Tienda</p>
+            <p>Contacto</p>
+        </div>
+    )
+}
+export default NavBar
